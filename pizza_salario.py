b282c62c98e2a1a6043fc346f5cc0181fbc64d29
cristianoalmeida <69701333+cristianoalmeida@users.noreply.github.com>
2020-08-14 20:11:30 -0400

    Add files via upload

---
+++ pizza_salario.py
@@ -0,0 +1,5 @@
+salario = float (input ('Digite seu sálario: '))
+print ('Essencial:', salario*55//100)
+print ('Poupar:', salario*30//100)
+print ('Livre:', salario*10//100)
+print ('Educação:', salario*5//100)
